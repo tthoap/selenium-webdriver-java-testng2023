151c6834e4569e3ee04997d48802b84fdaf672e7
ad <tthoap>
2024-01-01 20:42:42 +0700

    Topic 07 default dropdown

---
+++ selenium/webdriver/Topic_07_Default_dropdown.java
@@ -0,0 +1,122 @@
+package webdriver;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_07_Default_dropdown {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	Select select;
+	String email, firstName, lastName, companyName, day, month, year;
+
+//edit
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		email = "Angle" + generateEmail() + "@mail.net";
+		firstName = "Hoatran";
+		lastName = "auto";
+		companyName = "sams";
+		day = "10";
+		month = "June";
+		year = "1990";
+
+	}
+
+	@Test
+	public void TC_01_Nopcommerce() {
+		driver.get("https://demo.nopcommerce.com/");
+
+		driver.findElement(By.className("ico-register")).click();
+		driver.findElement(By.id("FirstName")).sendKeys(firstName);
+		driver.findElement(By.id("LastName")).sendKeys(lastName);
+
+		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		select.selectByVisibleText(day);
+
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), day);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		select.selectByVisibleText(month);
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), month);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthYear")));
+		select.selectByVisibleText(year);
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), year);
+
+		driver.findElement(By.id("Email")).sendKeys(email);
+		driver.findElement(By.id("Company")).sendKeys(companyName);
+		driver.findElement(By.id("Password")).sendKeys("12345678");
+		driver.findElement(By.id("ConfirmPassword")).sendKeys("12345678");
+		driver.findElement(By.id("register-button")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='result' and text()='Your registration completed']")).isDisplayed());
+		//sleepInSecond(2000);
+		//login
+		driver.findElement(By.className("ico-login")).click();
+		driver.findElement(By.id("Email")).sendKeys(email);
+		driver.findElement(By.id("Password")).sendKeys("12345678");
+		driver.findElement(By.cssSelector(".login-button")).click();
+		
+		
+		//sleepInSecond(2000);
+		
+		driver.findElement(By.className("ico-account")).click();
+		Assert.assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), firstName);
+		Assert.assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), lastName);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), day);
+		select = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), month);
+		select = new Select(driver.findElement(By.name("DateOfBirthYear")));
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), year);
+		
+		Assert.assertEquals(driver.findElement(By.id("Company")).getAttribute("value"), companyName);
+	}
+
+	@Test
+	public void TC_02_() {
+	}
+
+	@Test
+	public void TC_03_() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int generateEmail() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+	public void sleepInSecond(long timeoutInSecond) {
+		try {
+			Thread.sleep(timeoutInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
